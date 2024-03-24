2808f845f53bfbdf70129b3b5e9323a4c25ad563
DCollins23 <148885778+DCollins23@users.noreply.github.com>
2024-03-24 15:53:18 -0400

    lesson 4

---
+++ js4/js/main.js
@@ -0,0 +1,14 @@
+// Numbers
+
+// An integer is a whole number
+const myNumber = 42;
+const myFloat = 42.01;
+const myString = "42.11212123abc";
+
+// Number methods
+// The Number.isInteger() method determines wheter the passed value is an integer.
+
+// The Number.parseFloat() method parses an argument and returns a floating point number
+// If a number cannot be parsed, it returns NaN.
+
+console.log(isNaN("Davey"));
