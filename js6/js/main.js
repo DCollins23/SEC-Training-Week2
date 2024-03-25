cb61b3b1c951b40d49a6a73e4c7f6543b5c52697
DCollins23 <148885778+DCollins23@users.noreply.github.com>
2024-03-25 18:11:14 -0400

    lesson 6

---
+++ js6/js/main.js
@@ -0,0 +1,6 @@
+// Your First Code Challenge
+
+// Write code that will return a random letter from your name
+const anyName = "Davey";
+index = (Math.floor(Math.random() * anyName.length));
+console.log(anyName.charAt(index));
