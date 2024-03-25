b850ea62fd270a2542c235ee5c840182fab46f5e
DCollins23 <148885778+DCollins23@users.noreply.github.com>
2024-03-25 18:32:22 -0400

    lesson 8

---
+++ js8/js/main.js
@@ -0,0 +1,37 @@
+// Conditionals: Switch Statements
+
+let playerOne = "scissors";
+let computer = "paper";
+
+switch (playerOne) {
+
+  case computer:
+    console.log("Tie game!");
+    break;
+
+  case "rock":
+    if (computer === "paper") {
+      console.log("computer wins!")
+    }
+    else {
+      console.log("playerOne wins!")
+    }
+    break;
+
+  case "paper":
+    if (computer === "scissors") {
+      console.log("computer wins!")
+    }
+    else {
+      console.log("playerOne wins!")
+    }
+    break;
+
+  default:
+    if (computer === "rock") {
+      console.log("computer wins!")
+    }
+    else {
+      console.log("playerOne wins!")
+    }
+}
