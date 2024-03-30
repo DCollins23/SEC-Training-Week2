c20d7f909d17b476a4b3fb0fd6117e0a6c9f068a
DCollins23 <148885778+DCollins23@users.noreply.github.com>
2024-03-30 16:51:30 -0400

    lesson 13

---
+++ js13/js/main.js
@@ -0,0 +1,9 @@
+// Functions
+
+// Methods = Built-in Functions
+
+const toProperCase = (name) => {
+  return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+}
+
+console.log(toProperCase("dAvEy"));
